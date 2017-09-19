55763761cd1a74be3f99150f6d1b7a3cc9ef4fa4
Damilare Joseph <dre4success@live.com>
2017-09-19 15:37:24 +0100

    importing child component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import axios from "axios";
+import AlbumDetail from "./AlbumDetail";
 
 class AlbumList extends Component {
   state = { albums: [] };
@@ -10,17 +11,14 @@ class AlbumList extends Component {
       .get("https://rallycoding.herokuapp.com/api/music_albums")
       .then(response => this.setState({ albums: response.data }));
   }
-    renderAlbums() {
-       return this.state.albums.map(album => 
-       <Text key={album.title}>{album.title}</Text>)
-    }
+  renderAlbums() {
+    return this.state.albums.map(album => (
+      <AlbumDetail key={album.title} album={album} />
+    ));
+  }
   render() {
-      console.log(this.state)
-    return (
-      <View>
-        {this.renderAlbums()}
-      </View>
-    );
+    // console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
