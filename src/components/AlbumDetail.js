523d173fb9f4fc190488e265cd81f8c740896800
Damilare <dre4success@live.com>
2017-09-20 10:59:53 +0100

    styling button and onPress()

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,23 +2,33 @@ import React from "react";
 import { Text, View, Image } from "react-native";
 import Card from "./card";
 import CardSection from "./CardSection";
+import Button from "./button";
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const {thumbnailStyle, headerContentStyle} = styles
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
-          <Image
-            style={thumbnailStyle}
-            source={{ uri: thumbnail_image }}
-          />
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{ uri: image }} />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
@@ -29,9 +39,23 @@ const styles = {
     flexDirection: "column",
     justifyContent: "space-around"
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
